6ad812329135ab44e41982f4b6444689a9db6326
Tomasz Buba <tomaszbuba.tb@gmail.com>
2014-10-30 22:51:02 +0100

    adding add check_all_size ( not finished)

--- bUtils.rb
+++ bUtils.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
-
+require 'find'
 def make_selector(path)
   #do something with path
   all= Dir.entries(path)
@@ -18,5 +18,33 @@ def make_selector(path)
 </selector>')
     out.close
   end
-
 end
+  def check_all_size(path)
+    #Check is there all sizes of every image in drawables
+    all = Hash.new
+      Dir["#{path}/*"].each do |something|
+     if File.directory?(something)
+        all.store(something,[])
+       end
+     Dir["#{something}/*"].each {|file| file.nil? ? nil: all[something]<<file}
+
+      end
+    puts all
+
+
+    end
+#need to work on it
+  def compare_arrays(*arrays)
+    arrays.
+    arrays.each do |tab,*tabs|
+        puts "tabs = #{tabs}"
+
+      end
+    end
+
+
+
+
+
+
+
